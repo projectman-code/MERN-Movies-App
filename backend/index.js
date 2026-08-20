559e08ba619a123d9e5fee4e1e916d0e8eff6225
adoptapets01 <adopta.pets01@gmail.com>
2026-08-20 07:17:41 +0530

    Add CORS support

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import path from "path";
+import cors from "cors";
 
 // Files
 import connectDB from "./config/db.js";
@@ -24,6 +25,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
